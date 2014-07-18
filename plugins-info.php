3e43a930a413f38dab34d7a28c24dd1874f94e2f
Nick Burne <nickburne@users.noreply.github.com>
2014-07-18 11:22:58 +0100

    Add horizontal sub-menu

--- plugins-info.php
+++ plugins-info.php
@@ -27,5 +27,10 @@ $pluginsInfo = array(
         'name' => 'Canvas Extension - Page Builder for Canvas',
         'latest-version' => '1.0',
         'description' => '<li>1.0 - Visual editor widget now bundled / New row visual styling options / Page Builder widget options / general bug fixes</li>'
-    )
+    ),
+    'cx-horizontal-sub-menu' => array(
+        'product-id' => 14337,
+        'name' => 'Canvas Extension - Horizontal Sub-menu',
+        'latest-version' => '1.0.1',
+        'description' => '<li>1.0.1 -Add compat with Center Menu and Logo with Full Width</li>'
 );
