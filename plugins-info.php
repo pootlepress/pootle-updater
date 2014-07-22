f92eb18c918ad1758d7ff3eaaa96656c6006e7c8
Nick Burne <nickburne@users.noreply.github.com>
2014-07-22 14:18:29 +0100

    Update plugins-info.php

--- plugins-info.php
+++ plugins-info.php
@@ -7,14 +7,14 @@ $pluginsInfo = array(
     'cx-align-menu-right-master' => array(
         'product-id' => 10896,
         'name' => 'Canvas Extension - Align Menu Right',
-        'latest-version' => '1.0.0',
-        'description' => '<li>Update 1</li><li>Update 2</li>'
+        'latest-version' => '1.1',
+        'description' => '<li>1.1 - ability to update within WordPress</li>'
     ),
     'cx-sticky-nav-master' => array(
         'product-id' => 10897,
         'name' => 'Canvas Extension - Sticky Nav',
-        'latest-version' => '1.0.0',
-        'description' => 'Make primary navigation sticky'
+        'latest-version' => '1.2',
+        'description' => '<li>v1.2 - ability to update within WordPress</li>'
     ),
     'cx-page-customizer-master' => array(
         'product-id' => 14480,
@@ -25,13 +25,44 @@ $pluginsInfo = array(
     'page-builder-for-canvas-master' => array(
         'product-id' => 13731,
         'name' => 'Canvas Extension - Page Builder for Canvas',
-        'latest-version' => '1.0',
-        'description' => '<li>1.0 - Visual editor widget now bundled / New row visual styling options / Page Builder widget options / general bug fixes</li>'
+        'latest-version' => '1.1',
+        'description' => '<li>1.1 - ability to update within WordPress</li>'
     ),
     'cx-horizontal-sub-menu-master' => array(
         'product-id' => 14337,
         'name' => 'Canvas Extension - Horizontal Sub-menu',
         'latest-version' => '1.1',
         'description' => '<li>v1.1 - Ability to get update notifications & update within WordPress</li><li>1.0.1 -Add compat with Center Menu and Logo with Full Width</li>'
+    ),
+    'cx-top-nav-manager-master' => array(
+        'product-id' => 13610,
+        'name' => 'Canvas Extension - Top Nav Manager',
+        'latest-version' => '1.3',
+        'description' => '<li>1.3 - Ability to get update notifications & update within WordPress</li>'
+    ),
+     'cx-masonry-blog-master' => array(
+        'product-id' => 13403,
+        'name' => 'Canvas Extension - Masonry Blog',
+        'latest-version' => '1.3',
+        'description' => '<li>1.3 - Ability to get update notifications & update within WordPress</li>'
+    ),
+    'cx-featured-video-for-slider-master' => array(
+        'product-id' => 13145,
+        'name' => 'Canvas Extension - Featured Video for Slider',
+        'latest-version' => '1.2',
+        'description' => '<li>1.2 - Ability to get update notifications & update within WordPress</li>'
+    ),
+    'cx-font-awesome-menus-for-canvas-master' => array(
+        'product-id' => 12899,
+        'name' => 'Canvas Extension - Font Awesome Menus for Canvas',
+        'latest-version' => '1.2',
+        'description' => '<li>1.2 - Ability to get update notifications & update within WordPress</li>'
+    )
+    ,
+    'cx-mobile-menu-manager-master' => array(
+        'product-id' => 12702,
+        'name' => 'Canvas Extension - Mobile Menu Manager',
+        'latest-version' => '1.1',
+        'description' => '<li>1.1 - Ability to get update notifications & update within WordPress</li>'
     )
 );
