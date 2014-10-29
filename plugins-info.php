b8d9ca55a4bd0bc9079dee61c48438dd094818c2
Nick Burne <nickburne@users.noreply.github.com>
2014-10-29 10:30:01 +0000

    Update plugins-info.php

--- plugins-info.php
+++ plugins-info.php
@@ -25,8 +25,8 @@ $pluginsInfo = array(
     'cx-page-customizer-master' => array(
         'product-id' => 14480,
         'name' => 'Canvas Extension - Page Customizer',
-        'latest-version' => '1.2.3',
-        'description' => '<li>1.2.3 - fix hide page title effecting post loop</li><li>1.2.2 - fix padding issues in mobile</li><li>1.2.1 - Fix "Pages & posts" options</li><li>1.2 - add ability to update in WordPress dashboard, Fix center alignment in mobile</li><li>1.1 - fix layout saving issue, bg color not over-riding & header image issue</li><li>v1.0 - Initial Release</li>'
+        'latest-version' => '1.3',
+        'description' => '<a href="http://www.pootlepress.com/change-logs/canvas-page-customizer-change-log/" target="_blank">View change log</a>'
     ),
     'page-builder-for-canvas-master' => array(
         'product-id' => 13731,
