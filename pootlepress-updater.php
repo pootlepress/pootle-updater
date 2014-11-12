d58e60e7d2820546b223e54d69e304bb3e14e7eb
Nick Burne <nickburne@users.noreply.github.com>
2014-11-12 10:42:45 +0000

    Update pootlepress-updater.php

--- pootlepress-updater.php
+++ pootlepress-updater.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: PootlePress Updater
 Description: Updater for PootlePress Server
-Version: 1.0.18
+Version: 1.1.0
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
